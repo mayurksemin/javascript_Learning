6f0d28a7f3f9060075a298ba26e7f9fc319ccd38
mayurksemin <mayur@ksemin.in>
2023-12-12 12:30:41 +0530

    dataconversition

---
+++ 03_DatatypeConversition.js
@@ -0,0 +1,11 @@
+let score="22"
+
+console.log(typeof score)  //this tell's that the which type of value is 
+
+
+let value_of_score=Number(score) //this convert the string into the Number
+
+
+console.log(typeof value_of_score)
+
+console.log(value_of_score)
