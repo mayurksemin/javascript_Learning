c91da360906f41dfbcbdb5850d28c85ad3c638c3
mayurksemin <mayur@ksemin.in>
2023-12-12 21:49:07 +0530

    Non-primitive datatype

---
+++ Datatype.js
@@ -0,0 +1,24 @@
+//primitive 
+
+// 7 type :String ,Number,Boolean,null,undefined,symbol,bigint
+
+
+const id=Symbol('23')
+
+
+//non-primitive
+
+//array , object ,function
+
+const Heros =["mayur","satish","chipade"]; //array
+
+let myObj={
+    name:"mayur",    //object in js
+    age:23
+}
+
+const myFunction=function(){   //function in javascript
+    console.log("Hello");
+}
+
+
