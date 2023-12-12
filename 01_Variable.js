190ef35aebce06f8c521683caf6d128f1f3a183c
mayurksemin <mayur@ksemin.in>
2023-12-12 12:10:13 +0530

    first comment for variable and data type

---
+++ 01_Variable.js
@@ -0,0 +1,12 @@
+const account_Id =505
+let accountName="Mayur"
+var accountPass="12345"
+accountCity="pune"
+let accountState;
+
+/*
+prefer to use let not to use var bacause of scope
+*/
+console.log(account_Id)
+
+console.table([account_Id,accountName,accountPass,accountCity,accountState])
---
+++ 02_dataType.js
@@ -0,0 +1,16 @@
+let name="mayur satish chipade";
+let age=22;
+let isLoggedIn=false
+
+
+
+/*number => 2 to power of 53
+bigint
+string =""
+boolean=>true/false
+null=>stantalone value
+undefined=> value not set
+symbole=>unique
+*/
+
+console .log(typeof undefined)
